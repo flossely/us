0452795765b7637c811395d115026966a2d89c5a
Karl August Nestor <jesuscaesar@outlook.com>
2022-12-10 13:16:55 +1000

    Latest update

---
+++ us.diction.php
@@ -0,0 +1,63 @@
+<?php
+
+$diction['us'] =
+[
+    'digital' =>
+    [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9'
+    ],
+    'alphabet' =>
+    [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',            
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'
+    ],
+    'action' =>
+    [
+        '100' => 'earned',
+        '200' => 'ended turn',
+        '300' => 'cannot access',
+        '400' => 'cannot reach',
+        '500' => 'is dead'
+    ],
+    'transaction' =>
+    [
+        '00' => 'TRANSACTION_APPROVED',
+        '12' => 'INVALID_TRANSACTION',
+        '30' => 'FORMAT_ERROR',
+        '51' => 'INSUFFICIENT_FUNDS'
+    ],
+];
+
