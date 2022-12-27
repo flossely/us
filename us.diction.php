d237cb82e708d5fb2a50b72209db9d48688d3b7a
Karl August Nestor <jesuscaesar@outlook.com>
2022-12-27 05:21:15 +0000

    Latest update

--- us.diction.php
+++ us.diction.php
@@ -15,6 +15,12 @@ $diction['us'] =
         '8',
         '9'
     ],
+    'mode' =>
+    [
+        -1 => 'Left',
+        0 => 'Center',
+        1 => 'Right'
+    ],
     'alphabet' =>
     [
         'a',
@@ -60,4 +66,3 @@ $diction['us'] =
         '51' => 'INSUFFICIENT_FUNDS'
     ],
 ];
-
